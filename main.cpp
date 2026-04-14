99fbcde3c2eb2638a01d921a38c523120afdf0a7
malharrrrrr <malhar.vikson@gmail.com>
2026-04-15 00:26:54 +0530

    2d map working

--- main.cpp
+++ main.cpp
@@ -1,26 +1,3 @@
-/*
-Copyright (c) 2004-2019, Lode Vandevenne
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #include <cmath>
 #include <string>
 #include <vector>
@@ -28,12 +5,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "quickcg.h"
 using namespace QuickCG;
-
-/*
-g++ *.cpp -lSDL -O3 -W -Wall -ansi -pedantic
-g++ *.cpp -lSDL
-*/
-
 
 #define screenWidth 640
 #define screenHeight 480
@@ -44,249 +15,259 @@ g++ *.cpp -lSDL
 
 int worldMap[mapWidth][mapHeight]=
 {
-  {4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,7,7,7,7,7,7,7,7},
-  {4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
-  {4,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
-  {4,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
-  {4,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
-  {4,0,4,0,0,0,0,5,5,5,5,5,5,5,5,5,7,7,0,7,7,7,7,7},
-  {4,0,5,0,0,0,0,5,0,5,0,5,0,5,0,5,7,0,0,0,7,7,7,1},
-  {4,0,6,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
-  {4,0,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,7,7,1},
-  {4,0,8,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
-  {4,0,0,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,7,7,7,1},
-  {4,0,0,0,0,0,0,5,5,5,5,0,5,5,5,5,7,7,7,7,7,7,7,1},
-  {6,6,6,6,6,6,6,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
-  {8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
-  {6,6,6,6,6,6,0,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
-  {4,4,4,4,4,4,0,4,4,4,6,0,6,2,2,2,2,2,2,2,3,3,3,3},
-  {4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
-  {4,0,0,0,0,0,0,0,0,0,0,0,6,2,0,0,5,0,0,2,0,0,0,2},
-  {4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
-  {4,0,6,0,6,0,0,0,0,4,6,0,0,0,0,0,5,0,0,0,0,0,0,2},
-  {4,0,0,5,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
-  {4,0,6,0,6,0,0,0,0,4,6,0,6,2,0,0,5,0,0,2,0,0,0,2},
-  {4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
-  {4,4,4,4,4,4,4,4,4,4,1,1,1,2,2,2,2,2,2,3,3,3,3,3}
+{4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,7,7,7,7,7,7,7,7},
+{4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
+{4,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
+{4,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
+{4,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
+{4,0,4,0,0,0,0,5,5,5,5,5,5,5,5,5,7,7,0,7,7,7,7,7},
+{4,0,5,0,0,0,0,5,0,5,0,5,0,5,0,5,7,0,0,0,7,7,7,1},
+{4,0,6,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
+{4,0,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,7,7,1},
+{4,0,8,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
+{4,0,0,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,7,7,7,1},
+{4,0,0,0,0,0,0,5,5,5,5,0,5,5,5,5,7,7,7,7,7,7,7,1},
+{6,6,6,6,6,6,6,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
+{8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+{6,6,6,6,6,6,0,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
+{4,4,4,4,4,4,0,4,4,4,6,0,6,2,2,2,2,2,2,2,3,3,3,3},
+{4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
+{4,0,0,0,0,0,0,0,0,0,0,0,6,2,0,0,5,0,0,2,0,0,0,2},
+{4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
+{4,0,6,0,6,0,0,0,0,4,6,0,0,0,0,0,5,0,0,0,0,0,0,2},
+{4,0,0,5,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
+{4,0,6,0,6,0,0,0,0,4,6,0,6,2,0,0,5,0,0,2,0,0,0,2},
+{4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
+{4,4,4,4,4,4,4,4,4,4,1,1,1,2,2,2,2,2,2,3,3,3,3,3}
 };
 
 Uint32 buffer[screenHeight][screenWidth];
 
-int main(int /*argc*/, char */*argv*/[])
+int main(int argc, char* argv[])
 {
-  double posX = 22.0, posY = 11.5;  //x and y start position
-  double dirX = -1.0, dirY = 0.0; //initial direction vector
-  double planeX = 0.0, planeY = 0.66; //the 2d raycaster version of camera plane
+double posX=22,posY=11.5;
+double dirX=-1,dirY=0;
+double planeX=0,planeY=0.66;
 
-  double time = 0; //time of current frame
-  double oldTime = 0; //time of previous frame
+double time=0,oldTime=0;
 
-  std::vector<Uint32> texture[8];
-  for(int i = 0; i < 8; i++) texture[i].resize(texWidth * texHeight);
+std::vector<Uint32> texture[8];
+for(int i=0;i<8;i++) texture[i].resize(texWidth*texHeight);
 
-  screen(screenWidth,screenHeight, 0, "Raycaster");
-  screen(400, 400, 1, "2D Map");
+screen(screenWidth,screenHeight,0,"Raycaster");
 
-  //generate some textures
-#if 0
-  for(int x = 0; x < texWidth; x++)
-  for(int y = 0; y < texHeight; y++)
-  {
-    int xorcolor = (x * 256 / texWidth) ^ (y * 256 / texHeight);
-    //int xcolor = x * 256 / texWidth;
-    int ycolor = y * 256 / texHeight;
-    int xycolor = y * 128 / texHeight + x * 128 / texWidth;
-    texture[0][texWidth * y + x] = 65536 * 254 * (x != y && x != texWidth - y); //flat red texture with black cross
-    texture[1][texWidth * y + x] = xycolor + 256 * xycolor + 65536 * xycolor; //sloped greyscale
-    texture[2][texWidth * y + x] = 256 * xycolor + 65536 * xycolor; //sloped yellow gradient
-    texture[3][texWidth * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; //xor greyscale
-    texture[4][texWidth * y + x] = 256 * xorcolor; //xor green
-    texture[5][texWidth * y + x] = 65536 * 192 * (x % 16 && y % 16); //red bricks
-    texture[6][texWidth * y + x] = 65536 * ycolor; //red gradient
-    texture[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
-  }
-#else
- //generate some textures
-  unsigned long tw, th;
-  loadImage(texture[0], tw, th, "pics/eagle.png");
-  loadImage(texture[1], tw, th, "pics/redbrick.png");
-  loadImage(texture[2], tw, th, "pics/purplestone.png");
-  loadImage(texture[3], tw, th, "pics/greystone.png");
-  loadImage(texture[4], tw, th, "pics/bluestone.png");
-  loadImage(texture[5], tw, th, "pics/mossy.png");
-  loadImage(texture[6], tw, th, "pics/wood.png");
-  loadImage(texture[7], tw, th, "pics/colorstone.png");
-#endif
+unsigned long tw,th;
+loadImage(texture[0],tw,th,"pics/eagle.png");
+loadImage(texture[1],tw,th,"pics/redbrick.png");
+loadImage(texture[2],tw,th,"pics/purplestone.png");
+loadImage(texture[3],tw,th,"pics/greystone.png");
+loadImage(texture[4],tw,th,"pics/bluestone.png");
+loadImage(texture[5],tw,th,"pics/mossy.png");
+loadImage(texture[6],tw,th,"pics/wood.png");
+loadImage(texture[7],tw,th,"pics/colorstone.png");
 
-  //start the main loop
-  while(!done())
-  {
-    for(int x = 0; x < w; x++)
-    {
-      //calculate ray position and direction
-      double cameraX = 2 * x / (double)w - 1; //x-coordinate in camera space
-      double rayDirX = dirX + planeX*cameraX;
-      double rayDirY = dirY + planeY*cameraX;
+while(!done())
+{
 
-      //which box of the map we're in
-      int mapX = int(posX);
-      int mapY = int(posY);
+for(int x=0;x<w;x++)
+{
+double cameraX=2*x/(double)w-1;
+double rayDirX=dirX+planeX*cameraX;
+double rayDirY=dirY+planeY*cameraX;
 
-      //length of ray from current position to next x or y-side
-      double sideDistX;
-      double sideDistY;
+int mapX=int(posX);
+int mapY=int(posY);
 
-      //length of ray from one x or y-side to next x or y-side
-      double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
-      double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
-      double perpWallDist;
+double sideDistX,sideDistY;
 
-      //what direction to step in x or y-direction (either +1 or -1)
-      int stepX;
-      int stepY;
+double deltaDistX=(rayDirX==0)?1e30:fabs(1/rayDirX);
+double deltaDistY=(rayDirY==0)?1e30:fabs(1/rayDirY);
 
-      int hit = 0; //was there a wall hit?
-      int side; //was a NS or a EW wall hit?
+int stepX,stepY;
 
-      //calculate step and initial sideDist
-      if(rayDirX < 0)
-      {
-        stepX = -1;
-        sideDistX = (posX - mapX) * deltaDistX;
-      }
-      else
-      {
-        stepX = 1;
-        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
-      }
-      if(rayDirY < 0)
-      {
-        stepY = -1;
-        sideDistY = (posY - mapY) * deltaDistY;
-      }
-      else
-      {
-        stepY = 1;
-        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-      }
-      //perform DDA
-      while (hit == 0)
-      {
-        //jump to next map square, either in x-direction, or in y-direction
-        if(sideDistX < sideDistY)
-        {
-          sideDistX += deltaDistX;
-          mapX += stepX;
-          side = 0;
-        }
-        else
-        {
-          sideDistY += deltaDistY;
-          mapY += stepY;
-          side = 1;
-        }
-        //Check if ray has hit a wall
-        if(worldMap[mapX][mapY] > 0) hit = 1;
-      }
+int hit=0;
+int side;
 
-      //Calculate distance of perpendicular ray (Euclidean distance would give fisheye effect!)
-      if(side == 0) perpWallDist = (sideDistX - deltaDistX);
-      else          perpWallDist = (sideDistY - deltaDistY);
+if(rayDirX<0)
+{
+stepX=-1;
+sideDistX=(posX-mapX)*deltaDistX;
+}
+else
+{
+stepX=1;
+sideDistX=(mapX+1-posX)*deltaDistX;
+}
 
-      //Calculate height of line to draw on screen
-      int lineHeight = (int)(h / perpWallDist);
+if(rayDirY<0)
+{
+stepY=-1;
+sideDistY=(posY-mapY)*deltaDistY;
+}
+else
+{
+stepY=1;
+sideDistY=(mapY+1-posY)*deltaDistY;
+}
 
+while(hit==0)
+{
+if(sideDistX<sideDistY)
+{
+sideDistX+=deltaDistX;
+mapX+=stepX;
+side=0;
+}
+else
+{
+sideDistY+=deltaDistY;
+mapY+=stepY;
+side=1;
+}
 
-      int pitch = 100;
+if(worldMap[mapX][mapY]>0) hit=1;
+}
 
-      //calculate lowest and highest pixel to fill in current stripe
-      int drawStart = -lineHeight / 2 + h / 2 + pitch;
-      if(drawStart < 0) drawStart = 0;
-      int drawEnd = lineHeight / 2 + h / 2 + pitch;
-      if(drawEnd >= h) drawEnd = h - 1;
+double perpWallDist;
 
-      //texturing calculations
-      int texNum = worldMap[mapX][mapY] - 1; //1 subtracted from it so that texture 0 can be used!
+if(side==0) perpWallDist=(sideDistX-deltaDistX);
+else perpWallDist=(sideDistY-deltaDistY);
 
-      //calculate value of wallX
-      double wallX; //where exactly the wall was hit
-      if(side == 0) wallX = posY + perpWallDist * rayDirY;
-      else          wallX = posX + perpWallDist * rayDirX;
-      wallX -= floor((wallX));
+int lineHeight=int(h/perpWallDist);
 
-      //x coordinate on the texture
-      int texX = int(wallX * double(texWidth));
-      if(side == 0 && rayDirX > 0) texX = texWidth - texX - 1;
-      if(side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
+int drawStart=-lineHeight/2+h/2;
+if(drawStart<0) drawStart=0;
 
-      // TODO: an integer-only bresenham or DDA like algorithm could make the texture coordinate stepping faster
-      // How much to increase the texture coordinate per screen pixel
-      double step = 1.0 * texHeight / lineHeight;
-      // Starting texture coordinate
-      double texPos = (drawStart - pitch - h / 2 + lineHeight / 2) * step;
-      for(int y = drawStart; y < drawEnd; y++)
-      {
-        // Cast the texture coordinate to integer, and mask with (texHeight - 1) in case of overflow
-        int texY = (int)texPos & (texHeight - 1);
-        texPos += step;
-        Uint32 color = texture[texNum][texHeight * texY + texX];
-        //make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
-        if(side == 1) color = (color >> 1) & 8355711;
-        buffer[y][x] = color;
-      }
-    }
+int drawEnd=lineHeight/2+h/2;
+if(drawEnd>=h) drawEnd=h-1;
 
-    drawBuffer(buffer[0]);
-    for(int y = 0; y < h; y++) for(int x = 0; x < w; x++) buffer[y][x] = 0; //clear the buffer instead of cls()
-    //timing for input and FPS counter
-    oldTime = time;
-    time = getTicks();
-    double frameTime = (time - oldTime) / 1000.0; //frametime is the time this frame has taken, in seconds
-    print(1.0 / frameTime); //FPS counter
-    redraw();
+int texNum=worldMap[mapX][mapY]-1;
 
-    //speed modifiers
-    double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
-    double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
+double wallX;
 
-    readKeys();
-    //move forward if no wall in front of you
-    if(keyDown(SDLK_UP))
-    {
-      if(worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
-      if(worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
-    }
-    //move backwards if no wall behind you
-    if(keyDown(SDLK_DOWN))
-    {
-      if(worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
-      if(worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
-    }
-    //rotate to the right
-    if(keyDown(SDLK_RIGHT))
-    {
-      //both camera direction and camera plane must be rotated
-      double oldDirX = dirX;
-      dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
-      dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
-      double oldPlaneX = planeX;
-      planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
-      planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
-    }
-    //rotate to the left
-    if(keyDown(SDLK_LEFT))
-    {
-      //both camera direction and camera plane must be rotated
-      double oldDirX = dirX;
-      dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
-      dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
-      double oldPlaneX = planeX;
-      planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
-      planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
-    }
-    if(keyDown(SDLK_ESCAPE))
-    {
-      break;
-    }
-  }
+if(side==0) wallX=posY+perpWallDist*rayDirY;
+else wallX=posX+perpWallDist*rayDirX;
+
+wallX-=floor(wallX);
+
+int texX=int(wallX*texWidth);
+
+if(side==0 && rayDirX>0) texX=texWidth-texX-1;
+if(side==1 && rayDirY<0) texX=texWidth-texX-1;
+
+double step=1.0*texHeight/lineHeight;
+
+double texPos=(drawStart-h/2+lineHeight/2)*step;
+
+for(int y=drawStart;y<drawEnd;y++)
+{
+int texY=int(texPos)&(texHeight-1);
+texPos+=step;
+
+Uint32 color=texture[texNum][texHeight*texY+texX];
+
+if(side==1) color=(color>>1)&8355711;
+
+buffer[y][x]=color;
+}
+
+}
+
+/* ---------- MINIMAP ---------- */
+
+int scale=6;
+int offsetX=10;
+int offsetY=10;
+
+for(int my=0;my<mapHeight;my++)
+for(int mx=0;mx<mapWidth;mx++)
+{
+Uint32 color;
+
+if(worldMap[mx][my]>0)
+color=RGBtoINT(ColorRGB(255,255,255));
+else
+color=RGBtoINT(ColorRGB(0,0,0));
+
+for(int py=0;py<scale;py++)
+for(int px=0;px<scale;px++)
+buffer[offsetY+my*scale+py][offsetX+mx*scale+px]=color;
+}
+
+int px=int(posX*scale)+offsetX;
+int py=int(posY*scale)+offsetY;
+
+for(int y=-2;y<=2;y++)
+for(int x=-2;x<=2;x++)
+buffer[py+y][px+x]=RGBtoINT(ColorRGB(255,0,0));
+
+for(int i=0;i<10;i++)
+{
+int dx=int((posX+dirX*i*0.3)*scale)+offsetX;
+int dy=int((posY+dirY*i*0.3)*scale)+offsetY;
+
+buffer[dy][dx]=RGBtoINT(ColorRGB(0,255,0));
+}
+
+/* ----------------------------- */
+
+drawBuffer(buffer[0]);
+
+for(int y=0;y<h;y++)
+for(int x=0;x<w;x++)
+buffer[y][x]=0;
+
+oldTime=time;
+time=getTicks();
+
+double frameTime=(time-oldTime)/1000.0;
+
+print(1.0/frameTime);
+
+redraw();
+
+double moveSpeed=frameTime*5.0;
+double rotSpeed=frameTime*3.0;
+
+readKeys();
+
+if(keyDown(SDLK_UP))
+{
+if(!worldMap[int(posX+dirX*moveSpeed)][int(posY)]) posX+=dirX*moveSpeed;
+if(!worldMap[int(posX)][int(posY+dirY*moveSpeed)]) posY+=dirY*moveSpeed;
+}
+
+if(keyDown(SDLK_DOWN))
+{
+if(!worldMap[int(posX-dirX*moveSpeed)][int(posY)]) posX-=dirX*moveSpeed;
+if(!worldMap[int(posX)][int(posY-dirY*moveSpeed)]) posY-=dirY*moveSpeed;
+}
+
+if(keyDown(SDLK_RIGHT))
+{
+double oldDirX=dirX;
+dirX=dirX*cos(-rotSpeed)-dirY*sin(-rotSpeed);
+dirY=oldDirX*sin(-rotSpeed)+dirY*cos(-rotSpeed);
+
+double oldPlaneX=planeX;
+planeX=planeX*cos(-rotSpeed)-planeY*sin(-rotSpeed);
+planeY=oldPlaneX*sin(-rotSpeed)+planeY*cos(-rotSpeed);
+}
+
+if(keyDown(SDLK_LEFT))
+{
+double oldDirX=dirX;
+dirX=dirX*cos(rotSpeed)-dirY*sin(rotSpeed);
+dirY=oldDirX*sin(rotSpeed)+dirY*cos(rotSpeed);
+
+double oldPlaneX=planeX;
+planeX=planeX*cos(rotSpeed)-planeY*sin(rotSpeed);
+planeY=oldPlaneX*sin(rotSpeed)+planeY*cos(rotSpeed);
+}
+
+if(keyDown(SDLK_ESCAPE)) break;
+
+}
+
+return 0;
 }
---
+++ readme.md
@@ -0,0 +1,196 @@
+# retro 3d maze engine (raycasting)
+
+a small retro-style 3d engine that renders a maze using **raycasting** (similar to wolfenstein 3d).
+
+you can move inside the maze using arrow keys.
+
+---
+
+# 1. install required tools
+
+## step 1 — install msys2
+
+download:
+👉 [https://www.msys2.org/](https://www.msys2.org/)
+
+install using default settings.
+
+after install:
+open **msys2 ucrt64 terminal**
+
+---
+
+## step 2 — install compiler and sdl
+
+run:
+
+```
+pacman -S mingw-w64-ucrt-x86_64-gcc
+pacman -S mingw-w64-ucrt-x86_64-sdl
+```
+
+press **y** when asked.
+
+this installs:
+
+* g++ compiler
+* sdl graphics library
+
+---
+
+# 2. download project files
+
+create a folder anywhere, for example:
+
+```
+desktop/maze
+```
+
+place these files inside:
+
+```
+maze/
+│
+├── main.cpp
+├── quickcg.cpp
+├── quickcg.h
+│
+└── pics/
+    ├── eagle.png
+    ├── redbrick.png
+    ├── purplestone.png
+    ├── greystone.png
+    ├── bluestone.png
+    ├── mossy.png
+    ├── wood.png
+    └── colorstone.png
+```
+
+quickcg files download:
+
+[https://lodev.org/cgtutor/files/quickcg.cpp](https://lodev.org/cgtutor/files/quickcg.cpp)
+[https://lodev.org/cgtutor/files/quickcg.h](https://lodev.org/cgtutor/files/quickcg.h)
+
+textures are required or the program will crash.
+
+---
+
+# 3. open project folder in terminal
+
+open **msys2 ucrt64 terminal**
+
+navigate to project folder:
+
+example:
+
+```
+cd /c/users/yourname/desktop/maze
+```
+
+---
+
+# 4. compile
+
+run:
+
+```
+g++ *.cpp -lmingw32 -lsdlmain -lsdl -o3 -o raycaster
+```
+
+if compilation succeeds, a file appears:
+
+```
+raycaster.exe
+```
+
+ignore warning about missing return statement.
+
+---
+
+# 5. run program
+
+```
+./raycaster.exe
+```
+
+a window opens showing a 3d maze.
+
+---
+
+# 6. controls
+
+| key | action        |
+| --- | ------------- |
+| ↑   | move forward  |
+| ↓   | move backward |
+| ←   | turn left     |
+| →   | turn right    |
+| esc | exit          |
+
+---
+
+# 7. project structure explanation
+
+| file        | purpose                   |
+| ----------- | ------------------------- |
+| main.cpp    | raycasting engine         |
+| quickcg.cpp | graphics helper functions |
+| quickcg.h   | declarations for quickcg  |
+| pics/       | wall textures             |
+
+quickcg simplifies:
+
+* window creation
+* keyboard input
+* image loading
+* drawing pixels
+
+---
+
+# 8. common errors
+
+### sdl not found
+
+run:
+
+```
+pacman -S mingw-w64-ucrt-x86_64-sdl
+```
+
+---
+
+### undefined reference to winmain
+
+compile using:
+
+```
+g++ *.cpp -lmingw32 -lsdlmain -lsdl -o3 -o raycaster
+```
+
+---
+
+### textures not loading
+
+check folder:
+
+```
+pics/
+```
+
+must be in same directory as exe.
+
+---
+
+# 9. what this program demonstrates
+
+concepts:
+
+* raycasting
+* dda algorithm
+* texture mapping
+* camera projection
+* game loop
+
+each vertical pixel column shoots a ray into a 2d grid map to simulate a 3d view.
+
+retro illusion, modern laptop, zero dragons harmed
